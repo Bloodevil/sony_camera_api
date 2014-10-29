5958045335c3b3c3b08f905f8521713994a74d75
Dante Alighieri <dante@Dantes-MacBook-Pro.local>
2014-10-29 16:17:57 +0900

    update setup file change dir structure.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import os
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 README = open(os.path.join(here, 'README.md')).read()
 
-version = '0.1.1'
+version = '0.1.2'
 
 install_requires = [
 ]
@@ -16,14 +16,16 @@ install_requires = [
 setup(name='sony-camera-api',
     version = version,
     description = "Sony Camera Remote API for python",
+    long_description = README,
     author='Yeaji Shin',
     author_email='yeahjishin@gmail.com',
     url='https://github.com/Bloodevil/sony_camera_api',
     download_url='https://github.com/Bloodevil/sony_camera_api/tarball/0.1.1',
     license='MIT',
     install_requires=install_requires,
-    py_modules=["sony_camera_api",
-    ],
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    py_modules=["pysony"],
     keywords=['sony', 'camera', 'remote', 'api'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
