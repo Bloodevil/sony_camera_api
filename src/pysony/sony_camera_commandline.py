6686f9fbec29e1a0750bcef90fdf846820a6b513
yeaji.shin <bloodevil4@gmail.com>
2014-10-29 16:34:32 +0900

    update commandline code.

--- src/pysony/sony_camera_commandline.py
+++ src/pysony/sony_camera_commandline.py
@@ -1,6 +1,6 @@
 import sys
 import pprint
-from sony_camera_api import sony_api
+from sony_camera_api import SonyAPI
 
 method = None
 param = []
@@ -10,5 +10,6 @@ if len(sys.argv) > 1:
 	if len(sys.argv) > 2:
 		param = list(sys.argv[2:])
 
+sony = SonyAPI()
 pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(sony_api(method, param))
+pp.pprint(sony._cmd(method, param=None))
