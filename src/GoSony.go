70e5ee02b1b1180ef6b8fb6a9a72a425160f1067
shinyeaji <yeaji@shinyeajis-MacBook-Pro.local>
2015-06-24 23:59:04 +0900

    add gosony.  add structure of sonycameraapi.

---
+++ src/GoSony.go
@@ -0,0 +1,54 @@
+//# Common Header
+//# 0--------1--------2--------+--------4----+----+----+----8
+//# |0xFF    |payload | sequence number | Time stamp        |
+//# |        |type    |                 |                   |
+//# +-------------------------------------------------------+
+//#
+//# Payload Header
+//# 0--------------------------4-------------------7--------8
+//# | Start code               |  JPEG data size   | Padding|
+//# +--------------------------4------5---------------------+
+//# | Reserved                 | 0x00 | ..                  |
+//# +-------------------------------------------------------+
+//# | .. 115[B] Reserved                                    |
+//# +-------------------------------------------------------+
+//# | ...                                                   |
+//# ------------------------------------------------------128
+//#
+//# Payload Data
+//# in case payload type = 0x01
+//# +-------------------------------------------------------+
+//# | JPEG data size ...                                    |
+//# +-------------------------------------------------------+
+//# | ...                                                   |
+//# +-------------------------------------------------------+
+//# | Padding data size ...                                 |
+//# ------------------------------JPEG data size + Padding data size
+
+package GoSony
+
+type CommonHeader struct {
+	offset          int8
+	Payload_type    int8
+	Sequence_number int16
+	Timestamp       uint32
+}
+
+type PayloadHeader struct {
+	Start_code     uint32
+	JPEG_data_size uint16
+	padding        int8
+	reserved_1     uint32
+	flag           int8
+	//reserved_2  129-13
+}
+
+type SonyCameraFormat struct {
+	Common  CommonHeader
+	Payload PayloadHeader
+	Data    []byte
+}
+
+func ParseSonyCameraData(rd io.Reader, dst *SonyCameraFormat) error {
+	return error
+}
