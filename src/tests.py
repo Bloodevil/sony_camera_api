0d1c189e958bb6f8519aabf04b0acdb6b6e9561c
bloodevil <bloodevil4@gmail.com>
2016-10-22 14:01:20 +0900

    delete sample test.

--- src/tests.py
+++ src/tests.py
@@ -6,13 +6,6 @@ class TestStringMethods(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
     def test_loadpysony(self):
         api = pysony.SonyAPI()
         self.assertEqual(api.QX_ADDR, 'http://10.0.0.1:10000')
